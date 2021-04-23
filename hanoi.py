28addee0af7dd5e0f7ca3ff12d8c21ea3510ccd0
NicolasFirmo <nicolasfofo@hotmail.com>
2021-04-23 13:35:01 -0300

    Colors!!!

--- hanoi.py
+++ hanoi.py
@@ -1,6 +1,8 @@
 from renderer import draw_cylinder
 from loop import Scene
 from time import sleep
+from random import randint
+import sys
 
 
 class Hanoi:
@@ -8,6 +10,8 @@ class Hanoi:
 
     def __init__(self, number_of_discs):
         self.__size = number_of_discs
+        self.__disc_colors = [(randint(0, 255), randint(
+            0, 255), randint(0, 255)) for _ in range(number_of_discs)]
         self.__towers = (list(range(number_of_discs, 0, -1)), [], [])
 
     def move(self, origin, destiny):
@@ -15,6 +19,8 @@ class Hanoi:
             self.__towers[destiny - 1].append(self.__towers[origin-1].pop())
         elif self.__towers[origin-1][-1] < self.__towers[destiny - 1][-1]:
             self.__towers[destiny - 1].append(self.__towers[origin-1].pop())
+        else:
+            print("Movimento inválido!", file=sys.stderr)
 
     def draw(self, surface, width, height):
         for index, tower in enumerate(self.__towers):
@@ -28,13 +34,22 @@ class Hanoi:
         y = pos[1]
         for disc in discs:
             draw_cylinder(surface, (pos[0], y), disc_height,
-                          small_radius * 2 * disc * growth_factor)
+                          small_radius * 2 * disc * growth_factor,
+                          self.__disc_colors[disc-1])
             y -= disc_height
+
+    def __len__(self):
+        return self.__size
+
 
 if __name__ == '__main__':
     hanoi = Hanoi(5)
-    Scene.set_draw_func(hanoi.draw)  # necessário para renderizar a torre na tela
 
+    # necessário para renderizar a torre na tela
+    Scene.set_draw_func(hanoi.draw)
+
+    sleep(1)
+    hanoi.move(1, 3)
     sleep(1)
     hanoi.move(1, 3)
     sleep(1)
